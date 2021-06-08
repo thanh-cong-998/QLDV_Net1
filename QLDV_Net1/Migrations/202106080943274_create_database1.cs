bc69c4079050ddce2cdcb21f795beb66a578586d
thanh-cong-998 <cong30071998@gmail.com>
2021-06-08 16:44:28 +0700

    up

---
+++ QLDV_Net1/Migrations/202106080943274_create_database1.cs
@@ -0,0 +1,16 @@
+﻿namespace QLDV_Net1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class create_database1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
